9348f453c24468be61deddf112fcf710fea46a0a
iampushkar <m4manishpushkar@gmail.com>
2024-10-12 20:33:22 +0530

    initial skeleton

---
+++ src/test/java/com/lld/pooling/PoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld.pooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
